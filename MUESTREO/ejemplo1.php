753f45c39f518b6901936b716a7033d350b7952b
Aarón <104464413+Programin123@users.noreply.github.com>
2025-06-05 16:13:46 -0600

    Add files via upload

---
+++ MUESTREO/ejemplo1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+        
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no , initial scale= 1.0, maximum-scale=1.0, minimum-scale=1.0">
+        
+        <title>PHP</title>
+        </head>
+<body bgcolor="gray" text="PINK">
+ <h1 align="center"> Tecnica de muestreo</h1>
+ <h2 align="center">PHP</h2>
+ <h3 align="center"><a href="/index.html">REGRESA AL INICIO AQUI</a></h3>
+ <hr>
+ <?PHP
+ echo"Hola Mundo <hr>";
+ echo 'adios mundo <br>';
+ echo '5to electronica <br>';
+ echo 'COMO TE AMIGAZO <br>';
+ ?>
+</body>
+</html>
